fd79ce3cafd73f5ab1ab191bedd329eaf186b177
Kail J <68143465+KailJ1@users.noreply.github.com>
2023-09-06 17:17:00 +0700

    Update Port-Scanner.py

--- Port-Scanner.py
+++ Port-Scanner.py
@@ -19,6 +19,10 @@ program_name = "Port Scanner"
 program_version = "1.2.2"
 program_description = "Это программа для сканирования портов."
 
+# Функция для очистки консоли
+def clear_console():
+    os.system("cls" if os.name == "nt" else "clear")
+
 # Функция для загрузки и обновления программы
 def download_update():
     try:
@@ -50,9 +54,12 @@ def download_update():
 # Функция для проверки обновлений
 def check_for_updates():
     try:
+        clear_console()  # Очистка консоли перед проверкой обновления
         print("Проверка обновлений...")
+
         update_url = f"{github_repository}/raw/main/news.txt"
         response = requests.get(update_url)
+
         if response.status_code == 200:
             remote_news = response.text.strip()
             with open("news.txt", "r") as file:
@@ -76,6 +83,8 @@ def check_for_updates():
                     choice = input("Введите '1' для скачивания обновления или '2' для продолжения без обновления: ")
                     if choice == '1':
                         download_update()
+                else:
+                    print("У вас уже новая версия.")  # Добавляем эту строку
             else:
                 print("У вас установлена последняя версия.")
         else:
@@ -87,43 +96,10 @@ def check_for_updates():
 # ...
 
 if __name__ == "__main__":
+    clear_console()  # Очистка консоли при запуске
+
     check_for_updates()  # Проверяем обновления при запуске
 
     while True:
-        os.system("cls" if os.name == "nt" else "clear")  # Очистка консоли при запуске
-
-        target = input("Введите IP-адрес или доменное имя для сканирования: ")
-        
-        try:
-            # Пытаемся разрешить доменное имя в IP-адрес
-            target_ip = socket.gethostbyname(target)
-            print(f"IP-адрес для сканирования: {target_ip}")
-        except socket.gaierror:
-            print(f"Не удалось разрешить доменное имя: {target}")
-            sys.exit(1)
-        
-        start_port = int(input("Введите начальный порт для сканирования (-1 для стандартных и 25560-25580): "))
-
-        if start_port != -1:
-            end_port = int(input("Введите конечный порт для сканирования: "))
-        else:
-            end_port = start_port  # Если указан порт -1, конечный порт также равен -1
-
-        print("Идёт сканирование...")  # Добавляем сообщение о начале сканирования
-
-        open_ports = scan_ports(target_ip, start_port, end_port)
-
-        if open_ports:
-            print("Активные порты:")
-            for port in open_ports:
-                service_name = get_service_name(port)
-                print(f"IP: {target_ip}, Port: {port}, Service: {service_name}")
-
-            log_scan_results(target_ip, open_ports)
-            print(f"Лог сохранён в папку: {os.path.abspath(log_directory)}/{log_filename}")
-        else:
-            print("На заданном IP нет активных портов.")
-        
-        continue_input = input("Введите '0' для продолжения или любой другой символ для завершения: ")
-        if continue_input != '0':
-            break
+        # Остальной код остается без изменений
+        # ...
