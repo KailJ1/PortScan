6dc6b7f7420449b2975e90fdd6747f0a96049b45
Kail J <68143465+KailJ1@users.noreply.github.com>
2023-09-06 17:46:57 +0700

    Update Port-Scanner.py

--- Port-Scanner.py
+++ Port-Scanner.py
@@ -2,95 +2,14 @@ import socket
 import datetime
 import logging
 import os
-import threading
-import concurrent.futures
-import sys
 import requests
-import shutil
-import zipfile
-import re
 
 # Глобальная переменная для хранения количества обработанных портов
 processed_ports = 0
 
-# Пример обновленных переменных
-github_repository = "https://github.com/KailJ1/PortScan"
-program_name = "Port Scanner"
-program_version = "1.2.3"
-program_description = "Это программа для сканирования портов."
-
 # Функция для очистки консоли
 def clear_console():
     os.system("cls" if os.name == "nt" else "clear")
-
-# Функция для загрузки и обновления программы
-def download_update():
-    try:
-        print("Загрузка обновления...")
-        update_url = f"{github_repository}/archive/main.zip"
-        response = requests.get(update_url)
-        if response.status_code == 200:
-            with open("update.zip", "wb") as file:
-                file.write(response.content)
-            print("Обновления успешно загружены. Обновление программы...")
-            update_directory = "update_temp"
-            with zipfile.ZipFile("update.zip", "r") as zip_ref:
-                zip_ref.extractall(update_directory)
-            # Заменяем существующие файлы новыми файлами
-            for root, dirs, files in os.walk(update_directory):
-                for file in files:
-                    src_file = os.path.join(root, file)
-                    dest_file = os.path.join(".", file)
-                    shutil.copy2(src_file, dest_file)
-            print("Программа успешно обновлена.")
-            # Удаляем временную папку с обновлениями и архив
-            shutil.rmtree(update_directory)
-            os.remove("update.zip")
-        else:
-            print("Не удалось загрузить обновления.")
-    except Exception as e:
-        print(f"Ошибка при загрузке и обновлении программы: {str(e)}")
-
-# Функция для проверки обновлений
-def check_for_updates():
-    try:
-        clear_console()  # Очистка консоли перед проверкой обновления
-        print("Проверка обновлений...")
-
-        update_url = f"{github_repository}/raw/main/news.txt"
-        response = requests.get(update_url)
-
-        if response.status_code == 200:
-            remote_news = response.text.strip()
-            with open("news.txt", "r") as file:
-                local_news = file.read().strip()
-            if remote_news != local_news:
-                # Проверяем версию программы в локальных новостях
-                local_version_match = re.search(r"Новая версия: (\d+\.\d+\.\d+)", local_news)
-                if local_version_match:
-                    local_version = local_version_match.group(1)
-                else:
-                    local_version = "0.0.0"
-                # Проверяем версию программы в удаленных новостях
-                remote_version_match = re.search(r"Новая версия: (\d+\.\d+\.\d+)", remote_news)
-                if remote_version_match:
-                    remote_version = remote_version_match.group(1)
-                else:
-                    remote_version = "0.0.0"
-                # Сравниваем версии и предлагаем обновление только при несоответствии версий
-                if remote_version != local_version:
-                    print("Доступно обновление. Желаете скачать обновление?")
-                    choice = input("Введите '1' для скачивания обновления или '2' для продолжения без обновления: ")
-                    if choice == '1':
-                        download_update()
-                else:
-                    print("У вас уже новая версия.")  # Добавляем эту строку
-            else:
-                print("У вас установлена последняя версия.")
-        else:
-            print("Не удалось проверить обновления.")
-    except Exception as e:
-        print(f"Ошибка при проверке обновлений: {str(e)}")
 
 # Функция для сканирования портов
 def scan_ports(target_ip, start_port, end_port):
@@ -140,10 +59,54 @@ def log_scan_results(target_ip, open_ports):
     except Exception as e:
         print(f"Ошибка при создании лога: {str(e)}")
 
+# Функция для проверки обновлений
+def check_for_updates():
+    try:
+        clear_console()  # Очистка консоли перед проверкой обновления
+        print("Проверка обновлений...")
+        
+        # Загрузка версии и изменений из файла Update.txt на GitHub
+        update_url = "https://raw.githubusercontent.com/KailJ1/PortScan/main/Update.txt"
+        response = requests.get(update_url)
+        
+        if response.status_code == 200:
+            remote_data = response.text.strip().split("\n")
+            remote_version = remote_data[0].split(": ")[1].strip()
+            
+            with open("Update.txt", "r") as local_file:
+                local_data = local_file.read().strip().split("\n")
+                local_version = local_data[0].split(": ")[1].strip()
+            
+            # Сравнение версий и вывод информации
+            if remote_version > local_version:
+                print(f"Доступна новая версия {remote_version}.")
+                print("\n".join(remote_data[1:]))
+                choice = input("Введите '1' для скачивания обновления или '2' для продолжения без обновления: ")
+                if choice == '1':
+                    download_update()
+            else:
+                print("У вас установлена последняя версия.")
+        else:
+            print("Не удалось проверить обновления.")
+    except Exception as e:
+        print(f"Ошибка при проверке обновлений: {str(e)}")
+
+# Функция для скачивания обновления
+def download_update():
+    try:
+        update_url = "https://github.com/KailJ1/PortScan/archive/main.zip"
+        response = requests.get(update_url)
+        if response.status_code == 200:
+            with open("update.zip", "wb") as zip_file:
+                zip_file.write(response.content)
+            print("Обновление успешно скачано. Запустите программу после распаковки.")
+        else:
+            print("Не удалось скачать обновление.")
+    except Exception as e:
+        print(f"Ошибка при скачивании обновления: {str(e)}")
+
 if __name__ == "__main__":
     clear_console()  # Очистка консоли при запуске
-
-    check_for_updates()  # Проверяем обновления при запуске
 
     while True:
         try:
