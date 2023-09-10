f4439903e49fd4c582e1e7c4a65749234e2f8170
Kail J <68143465+KailJ1@users.noreply.github.com>
2023-09-10 20:57:27 +0700

    Update Start.cmd

--- Start.cmd
+++ Start.cmd
@@ -1,3 +1,18 @@
 @echo off
-python Port-Scanner.py
-pause
+chcp 1251 > nul
+python -c "import requests" 2>nul
+if errorlevel 1 (
+    echo Установка библиотеки 'requests'...
+    pip install requests
+    if errorlevel 1 (
+        echo Не удалось установить библиотеку 'requests'. Пожалуйста, установите ее вручную и затем запустите программу.
+    ) else (
+        echo Библиотека 'requests' успешно установлена.
+        pause
+        python Port-Scanner.py
+    )
+) else (
+    echo Библиотека 'requests' уже установлена.
+    pause
+    python Port-Scanner.py
+)
