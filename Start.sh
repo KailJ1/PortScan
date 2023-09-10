8524d1102a45788e55f51050491596ea6cc19b87
Kail J <68143465+KailJ1@users.noreply.github.com>
2023-09-10 20:58:00 +0700

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-python3 Port-Scanner.py
+
+# Проверяем наличие библиотеки 'requests'
+python3 -c "import requests" 2>/dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Установка библиотеки 'requests'..."
+    pip3 install requests
+
+    if [ $? -ne 0 ]; then
+        echo "Не удалось установить библиотеку 'requests'. Пожалуйста, установите ее вручную и затем запустите программу."
+    else
+        echo "Библиотека 'requests' успешно установлена."
+        read -p "Нажмите Enter для продолжения..." pause
+        python3 Port-Scanner.py
+    fi
+else
+    echo "Библиотека 'requests' уже установлена."
+    read -p "Нажмите Enter для продолжения..." pause
+    python3 Port-Scanner.py
+fi
